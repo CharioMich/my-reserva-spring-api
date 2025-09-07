ef54faf4d83ef350f3dfb3d6ac44f076bf5f809c
Ch. Michalakis <mpampisb@gmail.com>
2025-09-07 14:39:52 +0300

    feat: add helper methods so Jackson can handle case-insensitive conversion of JSON

--- src/main/java/gr/aueb/cf/myreserva/core/enums/Role.java
+++ src/main/java/gr/aueb/cf/myreserva/core/enums/Role.java
@@ -1,6 +1,27 @@
 package gr.aueb.cf.myreserva.core.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Role {
     USER,
-    SUPER_ADMIN
+    SUPER_ADMIN;
+
+
+    /**
+     * Tells Jackson to use this method when deserializing JSON. Case-insensitive conversion achieved because Enums are UPPERCASE
+     */
+    @JsonCreator
+    public static Role fromString(String value) {
+        return Role.valueOf(value.toUpperCase());
+    }
+
+
+    /**
+     * Tells Jackson to use this method when serializing JSON. Role.USER → "user" because our frontend in React expects lowercase
+     */
+    @JsonValue
+    public String toValue() {
+        return this.name().toLowerCase();
+    }
 }
