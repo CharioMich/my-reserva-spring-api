94815dfc47dd7b34abd777bfd6248d6d1d7ecc2d
Ch. Michalakis <mpampisb@gmail.com>
2025-09-07 14:34:22 +0300

    feat: add mappings and transfer password encoding to auth service layer

--- src/main/java/gr/aueb/cf/myreserva/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/myreserva/mapper/Mapper.java
@@ -4,14 +4,12 @@ import gr.aueb.cf.myreserva.dto.*;
 import gr.aueb.cf.myreserva.model.Reservation;
 import gr.aueb.cf.myreserva.model.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class Mapper {
 
-    private final PasswordEncoder passwordEncoder;
 
     public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
         return new UserReadOnlyDTO(
@@ -24,6 +22,33 @@ public class Mapper {
                 user.getRole().toString()
         );
     }
+
+    public AuthenticationResponseDTO mapToAuthenticationResponseDTO(User user, String token) {
+        return new AuthenticationResponseDTO(
+                user.getUsername(),
+                user.getEmail(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getPhoneNumber(),
+                user.getRole().toString(),
+                token
+        );
+    }
+
+
+    public User mapToUserEntity(UserInsertDTO insertDTO) {
+        User user = new User();
+        user.setUsername(insertDTO.username());
+        user.setEmail(insertDTO.email());
+        user.setPassword(insertDTO.password());
+        user.setFirstname(insertDTO.firstname());
+        user.setLastname(insertDTO.lastname());
+        user.setPhoneNumber(insertDTO.phoneNumber());
+        user.setRole(insertDTO.role());
+
+        return user;
+    }
+
 
     public ReservationReadOnlyDTO mapToReservationReadOnlyDTO(Reservation reservation) {
         return new ReservationReadOnlyDTO(
