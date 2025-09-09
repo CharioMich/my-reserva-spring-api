f5cc07c2ed913a471f32f96779ad8f08c9b47372
Ch. Michalakis <mpampisb@gmail.com>
2025-09-09 16:07:06 +0300

    feat: add query-derived methods

--- src/main/java/gr/aueb/cf/myreserva/repository/UserRepository.java
+++ src/main/java/gr/aueb/cf/myreserva/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package gr.aueb.cf.myreserva.repository;
 
 import gr.aueb.cf.myreserva.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -8,4 +10,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(@Email(message = "Invalid email") String email);
+    boolean existsByPhoneNumber(@NotEmpty(message = "Phone number required") String phoneNumber);
 }
