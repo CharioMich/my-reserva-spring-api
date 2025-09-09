1b417abc2d22b1517153cb8027f7d5524e258a04
Ch. Michalakis <mpampisb@gmail.com>
2025-09-09 16:10:20 +0300

    feat: implement logic and functionality for updating users

--- src/main/java/gr/aueb/cf/myreserva/api/UserRestController.java
+++ src/main/java/gr/aueb/cf/myreserva/api/UserRestController.java
@@ -1,17 +1,24 @@
 package gr.aueb.cf.myreserva.api;
 
+import gr.aueb.cf.myreserva.core.exceptions.AppObjectAlreadyExists;
 import gr.aueb.cf.myreserva.core.exceptions.AppObjectNotFoundException;
-import gr.aueb.cf.myreserva.dto.UserReadOnlyDTO;
+import gr.aueb.cf.myreserva.core.exceptions.ValidationException;
+import gr.aueb.cf.myreserva.dto.user.UserInsertDTO;
+import gr.aueb.cf.myreserva.dto.user.UserReadOnlyDTO;
+import gr.aueb.cf.myreserva.dto.user.UserUpdateDTO;
 import gr.aueb.cf.myreserva.mapper.Mapper;
 import gr.aueb.cf.myreserva.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 
 @RestController
@@ -23,6 +30,11 @@ public class UserRestController {
     private final UserService userService;
     private final Mapper mapper;
 
+    /**
+     * GET CURRENT LOGGED-IN USER
+     * @param authentication When controller method has a parameter of type Authentication, Spring automatically resolves it from the current SecurityContext.
+     *                                 The Authentication object is created in the JwtAuthenticationFilter
+     */
     @GetMapping("/current")
     public ResponseEntity<UserReadOnlyDTO> getCurrentUser(Authentication authentication) throws AppObjectNotFoundException {
         try {
@@ -33,6 +45,36 @@ public class UserRestController {
             return ResponseEntity.ok(userReadOnlyDTO);
         } catch (AppObjectNotFoundException e) {
             LOGGER.warn("Could not get current user.", e);
+            throw e;
+        }
+    }
+
+
+    /**
+     * UPDATE CURRENT LOGGED-IN USER
+     */
+    @PutMapping("/current")
+    public ResponseEntity<UserReadOnlyDTO> updateCurrentUser(
+            @Valid @ModelAttribute UserUpdateDTO updateDTO,
+            Authentication authentication,
+            BindingResult bindingResult
+    )
+            throws AppObjectNotFoundException, ValidationException, AppObjectAlreadyExists, IOException {
+
+        String email = authentication.getName();
+
+        try {
+
+            if (bindingResult.hasErrors()) {
+                throw new ValidationException(bindingResult);
+            }
+
+            UserReadOnlyDTO userReadOnlyDTO = userService.updateUser(updateDTO, email);
+
+            return new ResponseEntity<>(userReadOnlyDTO, HttpStatus.OK);
+
+        } catch (Exception e) {
+            LOGGER.warn("User to update, with email:{} not found.", email);
             throw e;
         }
     }
--- src/main/java/gr/aueb/cf/myreserva/service/UserService.java
+++ src/main/java/gr/aueb/cf/myreserva/service/UserService.java
@@ -1,12 +1,19 @@
 package gr.aueb.cf.myreserva.service;
 
+import gr.aueb.cf.myreserva.core.exceptions.AppObjectAlreadyExists;
 import gr.aueb.cf.myreserva.core.exceptions.AppObjectNotFoundException;
-import gr.aueb.cf.myreserva.dto.UserReadOnlyDTO;
+import gr.aueb.cf.myreserva.dto.user.UserInsertDTO;
+import gr.aueb.cf.myreserva.dto.user.UserReadOnlyDTO;
+import gr.aueb.cf.myreserva.dto.user.UserUpdateDTO;
 import gr.aueb.cf.myreserva.mapper.Mapper;
 import gr.aueb.cf.myreserva.model.User;
 import gr.aueb.cf.myreserva.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +22,43 @@ public class UserService {
     private final UserRepository userRepository;
     private final Mapper mapper;
 
+    /**
+     * GET USER BY EMAIL (Get current User)
+     */
     public UserReadOnlyDTO getCurrentUser(String email) throws AppObjectNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new AppObjectNotFoundException("User", "User with email " + email + " not found."));
         return mapper.mapToUserReadOnlyDTO(user);
+    }
+
+
+    /**
+     * UPDATE CURRENT USER
+     */
+    @Transactional(rollbackOn = {AppObjectNotFoundException.class, IOException.class}) // In case of runtime exceptions or specified checked exceptions, rollback.
+    public UserReadOnlyDTO updateUser(UserUpdateDTO updateDTO, String email) throws AppObjectNotFoundException, IOException, AppObjectAlreadyExists {
+
+        User existingUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found."));
+
+        // Check if email or phone number already exist in some other user's data
+        if (userRepository.existsByEmail(updateDTO.email())
+                && !existingUser.getEmail().equals(updateDTO.email())) {
+            throw new AppObjectAlreadyExists("User", "Email already in use: " + updateDTO.email());
+        }
+        if (userRepository.existsByPhoneNumber(updateDTO.phoneNumber())
+                && !existingUser.getPhoneNumber().equals(updateDTO.phoneNumber())) {
+            throw new AppObjectAlreadyExists("User", "Phone number already in use: " + updateDTO.phoneNumber());
+        }
+
+        // Update only the necessary fields from DTO if they are provided (not null)
+        if (updateDTO.firstname() != null) existingUser.setFirstname(updateDTO.firstname());
+        if (updateDTO.lastname() != null) existingUser.setLastname(updateDTO.lastname());
+        if (updateDTO.email() != null) existingUser.setEmail(updateDTO.email());
+        if (updateDTO.phoneNumber() != null) existingUser.setPhoneNumber(updateDTO.phoneNumber());
+
+        User savedUser = userRepository.save(existingUser);
+
+        return mapper.mapToUserReadOnlyDTO(savedUser);
     }
 }
